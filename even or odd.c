eea6b6987f7fa1fcdf6f847eef3cf9c0ec799ddc
alamin513 <108076684+alamin513@users.noreply.github.com>
2022-09-16 21:40:32 +0600

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter any number: ");
+    scanf("%d",&number);
+    if(number%2==0)
+    {
+        printf("%d is a even number",number);
+    }
+    else
+    {
+        printf("%d is a odd number",number);
+    }
+    return 0;
+}
